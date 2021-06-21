e9fcf077f3b4b80545601c67ab10d94ea8214315
devillkaow <69180063+devillkaow@users.noreply.github.com>
2021-06-21 09:43:32 +0700

    init

---
+++ TodoItem.cs
@@ -0,0 +1,7 @@
+﻿public class TodoItem
+{
+    public string Title { get; set; }
+    public bool IsTodo { get; set; }
+    public bool IsDone { get; set; }
+    public bool IsDoing { get; set; }
+}
